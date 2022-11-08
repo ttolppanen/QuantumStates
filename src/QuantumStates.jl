ba97d3291a4dceeadbd3f6c129ad8239f02448cd
ttolppanen <taneli.tolppanen@gmail.com>
2022-11-08 15:46:18 +0200

    unit testing

--- src/QuantumStates.jl
+++ src/QuantumStates.jl
@@ -1,9 +1,53 @@
 module QuantumStates
 
-    #d : dimension; e.g. with qubits d = 2
-    #L : number of systems;
+using SparseArrays
 
-    function onezero(d::Integer, L::Integer)
-        
-    end
+export onezero
+export zeroone
+export allone
+export singleone
+
+#d : dimension; e.g. with qubits d = 2
+#L : number of systems;
+
+function zerostate(d::Integer)
+    out = spzeros(d)
+    out[1] = 1.0
+    return complex(out)
 end
+
+function onestate(d::Integer)
+    out = spzeros(d)
+    out[2] = 1.0
+    return complex(out)
+end
+
+function onezero(d::Integer, L::Integer)
+    zero = zerostate(d)
+    one = onestate(d)
+    sites = [i % 2 == 1 ? one : zero for i in 1:L] #Makes an array something like this [[1, 0], [0, 1], [1, 0]...]
+    return kron(sites...)
+end
+
+function zeroone(d::Integer, L::Integer)
+    zero = zerostate(d)
+    one = onestate(d)
+    sites = [i % 2 == 1 ? zero : one for i in 1:L] #Makes an array something like this [[1, 0], [0, 1], [1, 0]...]
+    return kron(sites...)
+end
+
+function allone(d::Integer, L::Integer)
+    one = onestate(d)
+    sites = [one for _ in 1:L]
+    return kron(sites...)
+end
+
+function singleone(d::Integer, L::Integer, i::Integer)
+    zero = zerostate(d)
+    one = onestate(d)
+    sites = [zero for _ in 1:L]
+    sites[i] = one
+    return kron(sites...)
+end
+
+end #module
---
+++ test/runtests.jl
@@ -0,0 +1,35 @@
+using QuantumStates, Test
+using SparseArrays
+using LinearAlgebra
+
+@test QuantumStates.zerostate(3)' * QuantumStates.onestate(3) == 0.0
+@test zeroone(2, 2)' * onezero(2, 2) == 0.0
+@test onezero(3, 4)' * allone(3, 4) == 0.0
+
+@testset "Normalization" begin
+    d = 5; L = 5
+    @test norm(zeroone(d, L)) == 1.0
+    @test norm(onezero(d, L)) == 1.0
+    @test norm(allone(d, L)) == 1.0
+    @test norm(singleone(d, L, 2)) == 1.0
+end
+
+@testset "Dimension" begin
+    d = 3; L = 4
+    @test length(zeroone(d, L)) == d^L
+    @test length(onezero(d, L)) == d^L
+    @test length(allone(d, L)) == d^L
+    @test length(singleone(d, L, 1)) == d^L
+end
+
+function testtypes(state)
+    @test typeof(state) <: AbstractSparseArray
+    @test typeof(state) == typeof(complex(state))
+end
+@testset "Types" begin
+    d = 4; L = 4
+    testtypes(zeroone(d, L))
+    testtypes(onezero(d, L))
+    testtypes(allone(d, L))
+    testtypes(singleone(d, L, 1))
+end
