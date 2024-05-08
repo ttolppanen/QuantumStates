6cf259f93392ed13a0941cb6fb879ef04f346c3f
ttolppanen <taneli.tolppanen@gmail.com>
2024-05-08 12:09:15 +0300

    thermal_state from vec

--- src/DensityOperator.jl
+++ src/DensityOperator.jl
@@ -3,15 +3,26 @@
 export vec_from_density_op
 export thermal_state
 
-function thermal_state(H, T)
+function thermal_state(H::Matrix{<:Number}, T)
     # kb = 1.380649 * 10^(-23)
     B = 1.0 / T
     out = exp(-B * H)
     return out / tr(out)
+end
+function thermal_state(H::AbstractVector, T)
+    out = exp.(-H / T)
+    return out / sum(out)
 end
 
 function vec_from_density_op(rho::Matrix{<:Number})
     u, s, v = svd(rho)
     result = findfirst(cumsum(s) .> rand())
     return u[:, result]
+end
+# for diagonal density operator
+function vec_from_density_op(rho::AbstractVector)
+    result = findfirst(cumsum(real.(rho)) .> rand())
+    out = complex(spzeros(size(rho)))
+    out[result] = 1.0
+    return out
 end
